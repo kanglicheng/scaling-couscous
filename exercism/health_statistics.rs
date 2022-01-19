8da666dcb45970539f26e6a6d9e6522eb6d0ea9c
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2022-01-18 22:28:52 -0800

    add health stats

---
+++ exercism/health_statistics.rs
@@ -0,0 +1,40 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub struct User {
+    name: String,
+    age: u32,
+    weight: f32,
+}
+
+impl User {
+    pub fn new(name: String, age: u32, weight: f32) -> Self {
+        let new_user = User{
+            name: name,
+            age: age,
+            weight: weight
+        };
+        new_user
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn age(&self) -> u32 {
+        self.age
+    }
+
+    pub fn weight(&self) -> f32 {
+       self.weight
+    }
+
+    pub fn set_age(&mut self, new_age: u32) {
+        self.age = new_age;
+    }
+
+    pub fn set_weight(&mut self, new_weight: f32) {
+        self.weight = new_weight;
+    }
+}
